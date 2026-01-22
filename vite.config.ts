1898f337f3676e8f72dca6535bd0811a788057bd
ERIELTON DE PAULA <87424423+Erieltondepaula@users.noreply.github.com>
2026-01-22 14:02:30 -0300

    Salvar atualizacoes

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,10 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
+server: {
     host: "::",
     port: 8080,
+    open: true, // <--- ADICIONE ESSA LINHA
     hmr: {
       overlay: false,
     },
